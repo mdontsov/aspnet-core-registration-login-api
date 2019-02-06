81109431d3003f8d526cb26002b47a5065fa2e4c
mdontsov <maksim.dontsov@gmail.com>
2019-02-06 18:15:47 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,30 +8,13 @@ namespace WebApi
     {
         public static void Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-            .Enrich.FromLogContext()
-            .WriteTo.Console()
-            .CreateLogger();
-            
-            try {
-                Log.Information("Starting web host");
                 BuildWebHost(args).Run();
-                return 0;
-            } catch (Exception ex) {
-                Log.Fatal(ex, "Host terminated unexpectedly");
-                return 1;
-            } finally {
-                Log.CloseAndFlush();
-            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .UseUrls("http://localhost:4000")
-                .UseSerilog()
                 .Build();
     }
 }
